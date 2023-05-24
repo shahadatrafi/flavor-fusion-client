503f46e9a736f9507aab0d753ee3e37cc5e0b38b
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-24 13:50:03 +0600

    swiper add on category

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
 import SectionTitle from '../../Shared/SectionTitle/SectionTitle';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+
+// import required modules
+import { Pagination } from "swiper";
+
+import slider1 from '../../../assets/home/slide1.jpg'
+import slider2 from '../../../assets/home/slide2.jpg'
+import slider3 from '../../../assets/home/slide3.jpg'
+import slider4 from '../../../assets/home/slide4.jpg'
+import slider5 from '../../../assets/home/slide5.jpg'
 
 const Category = () => {
     return (
         <div className='container mx-auto'>
-            <SectionTitle></SectionTitle>
+            <SectionTitle
+                subHeading={"---From 11:00am to 10:00pm---"}
+                heading={'ORDER ONLINE'}
+            ></SectionTitle>
+            <div>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src={slider1} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slider2} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slider3} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slider4} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={slider5} alt="" />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/SectionTitle/SectionTitle.jsx
+++ src/Pages/Shared/SectionTitle/SectionTitle.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const SectionTitle = ({heading, subHeading}) => {
     return (
         <div className='text-center w-3/12 mx-auto mb-12'>
-            <h3 className='italic text-yellow-500 text-xl mb-4'>---From 11:00am to 10:00pm---</h3>
-            <h2 className='text-4xl uppercase border-y-4 py-4 '>ORDER ONLINE</h2>
+            <h3 className='italic text-yellow-500 text-xl mb-4'>{subHeading}</h3>
+            <h2 className='text-4xl uppercase border-y-4 py-4 '>{heading}</h2>
         </div>
     );
 };
