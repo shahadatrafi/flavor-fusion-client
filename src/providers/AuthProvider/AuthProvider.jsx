a19129fef70d73aa0751c9b1f85a67098ae9551f
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-06-06 01:16:31 +0600

    access token set in localStorage

--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -4,6 +4,7 @@ import { createContext } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
@@ -38,6 +39,19 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log(currentUser);
             setUser(currentUser);
+
+            // get and set token
+
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt', { email: currentUser.email })
+                    .then(data => {
+                        console.log(data.data.token)
+                        localStorage.setItem('access-token', data.data.token)
+                })
+            } else {
+                localStorage.removeItem('access-token')
+            }
+
             setLoading(false)
         })
         return () => {
